48857b172e0f2bb7b3eb220ee4ed81e4e9f591cb
adhithyan21 <premadhi509@gmail.com>
2026-06-22 20:38:12 +0530

    express server is working

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-const express = require('express');
+const exp = require('express');
+
+const app = exp();
+
+app.listen(3000 , (res, req) => {
+    console.log("Express Server is working on port 3000");
+});
+
+app.get('/', (req,res) => {
+    res.send("Server is running");
+});
+
+
+app.get('/Inventory', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/about', (req, res) => {
+    res.send("Welcome to express server");
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("Page not Found");
+})
+
