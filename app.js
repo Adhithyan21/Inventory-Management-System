52ea8d9efee5c2259ade73b0436d64aec1074a3f
adhithyan21 <premadhi509@gmail.com>
2026-06-29 20:50:39 +0530

    get methods implemented

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ app.listen(3000 , () => {
 });
 
 app.get('/', (req,res) => {
-    res.send("Server is running");
+    res.send("Welcome to inventory management system");
+    console.log("Server is running");
 });
 
 
-app.get('/Inventory', (req, res) => {
+app.get('/Inventorysystem', (req, res) => {
     res.redirect('/');
 });
 
-app.get('/about', (req, res) => {
-    res.send("Welcome to express server");
+app.get('/products', (req, res) => {
+    res.send(products.json);
 });
 
 
@@ -24,3 +25,26 @@ app.use((req, res) => {
     res.status(404).send("Page not Found");
 })
 
+const products = [
+    {
+        productId: 1,
+        "name": "Rice",
+        "category": "Food",
+        price: 50,
+        quantity: 50
+    },
+    {
+        productId: 2,
+        "name": "Dal",
+        "category": "Food",
+        price: 30,
+        quantity: 50
+    },
+    {
+        productId: 3,
+        "name": "Wheat",
+        "category": "Food",
+        price: 25,
+        quantity: 50
+    }
+];
